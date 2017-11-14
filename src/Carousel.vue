2273514a89b0943d7014f1ef59163687d7e8295c
Edgar Xolop <edgarxoix@gmail.com>
2017-11-14 01:11:15 -0600

    initial commit

---
+++ src/Carousel.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="hero is-primary is-large">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+        {{msg}}
+        </h1>
+        <h2 class="subtitle">
+        Medium subtitle
+        </h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'carousel',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style src="bulma/css/bulma.css"></style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import carousel from './Carousel.vue'
+
+new Vue({
+  el: '#app',
+  components:{
+    carousel
+  }
+})
